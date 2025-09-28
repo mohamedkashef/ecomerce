b200318298e025a118d11ee3c7b20dcdb07269ae
Mohamed Ragab <mohamed.ragab.kashef@gmail.com>
2025-09-28 07:53:08 +0300

    install fontawosum

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+  import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+  import { faCoffee } from '@fortawesome/free-solid-svg-icons'; // Import specific icons you need
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  faUser = faCoffee; // Example icon usage
 
 }
